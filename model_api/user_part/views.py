989ef2e8b0263d6825075550f6b747b752e9ef36
Aleksandr <malakhov.aleksandr.98@gmail.com>
2021-04-23 20:49:23 +0300

    1

--- model_api/user_part/views.py
+++ model_api/user_part/views.py
@@ -10,11 +10,11 @@ def personal(request):
     return HttpResponse(json.dumps(response_data), content_type="application/json")
 
 def get_file(request):
-    return FileResponse(open(os.path.join(STATICFILES_DIRS[0],'\\1.jpg'), 'rb'))
+    return FileResponse(open(os.path.join(STATICFILES_DIRS[0],'1.jpg'), 'rb'))
 
 def set_file(request):
     data = request.FILES['input_file'].read()
-    with open(os.path.join(STATICFILES_DIRS[0],'\\1.jpg'), 'wb') as fout:
+    with open(os.path.join(STATICFILES_DIRS[0],'1.jpg'), 'wb') as fout:
         fout.write(data)
     response_data = {'status': 'ok'}
     return HttpResponse(json.dumps(response_data), content_type="application/json")
