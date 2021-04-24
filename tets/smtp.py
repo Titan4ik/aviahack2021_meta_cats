89c7858d235c065e7eadc84ae9855fc6946d9d3c
Alexey Vinnikov <vin-nikov@yandex.ru>
2021-04-24 23:47:54 +0300

    modify smtp example

--- tets/smtp.py
+++ tets/smtp.py
@@ -3,31 +3,38 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 
+def send_mails(customer_email, producer_email, files, service_name):
+    mail_content = 'Вы успешно оформили услугу "' + service_name + '". Все подписанные и заполненные вами документы высланы в прикрепленных файлах.'
+    sender_address='mai.hackathon.mts.meta.cats@gmail.com'
+    
+    message = MIMEMultipart()
+    message['From'] = sender_address
+    message.attach(MIMEText(mail_content, 'plain'))
+
+    for pdffile in files:
+        with open(pdffile, "rb") as f:
+            attach = MIMEApplication(f.read(),_subtype="pdf")
+        attach.add_header('Content-Disposition','attachment',filename=str(pdffile))
+        message.attach(attach)
+
+    session = smtplib.SMTP('smtp.gmail.com', 587)
+    session.starttls()
+    session.login(sender_address, 'Hackathon1234#')
+    
+    message['Subject'] = 'Вами была оформлена услуга "' + service_name + '"'
+    message['To'] = customer_email
+    session.sendmail(sender_address, customer_email, message.as_string())
+    
+    message['Subject'] = 'Пользователь "' + customer_email + '" офрмил услугу "' + service_name + '"'
+    message['To'] = producer_email
+    session.sendmail(sender_address, producer_email, message.as_string())
+    
+    session.quit()
+    
+
 service_name = 'Услуга №1'
 files = ['file1.pdf', 'file2.pdf']
 receiver_address = ['vin-nikov@yandex.ru', '15252vlad@gmail.com']
 
-
-mail_subject = 'Вами была оформлена услуга "' + service_name + '"'
-mail_content = 'Вы успешно оформили услугу "' + service_name + '". Все подписанные и заполненные вами документы высланы в прикрепленных файлах.'
-sender_address='mai.hackathon.mts.meta.cats@gmail.com'
-message = MIMEMultipart()
-message['From'] = sender_address
-message['Subject'] = mail_subject
-
-message.attach(MIMEText(mail_content, 'plain'))
-
-for pdffile in files:
-    with open(pdffile, "rb") as f:
-        attach = MIMEApplication(f.read(),_subtype="pdf")
-    attach.add_header('Content-Disposition','attachment',filename=str(pdffile))
-    message.attach(attach)
-
-session = smtplib.SMTP('smtp.gmail.com', 587)
-session.starttls()
-session.login(sender_address, 'Hackathon1234#')
-for rec in receiver_address:
-    message['To'] = rec
-    session.sendmail(sender_address, rec, message.as_string())
-session.quit()
+send_mails(receiver_address[0], receiver_address[1], files, service_name)
 
