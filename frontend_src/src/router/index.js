8c3b6929b642363bca58bd4fb50c5a0a322f68a8
Alex Vinnikov <vin-nikov@yandex.ru>
2021-04-24 05:35:52 +0300

    add some func at client

--- frontend_src/src/router/index.js
+++ frontend_src/src/router/index.js
@@ -1,22 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/create-service',
-    name: 'CreateService',
-    component: () => import('../views/CreateService.vue')
-  }
+const routes = [{
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/create-service',
+        name: 'CreateService',
+        component: () =>
+            import ('../views/CreateService.vue')
+    },
+    {
+        path: '/get-service',
+        name: 'GetService',
+        component: () =>
+            import ('../views/GetService.vue')
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
+    history: createWebHistory(),
+    routes
 })
 
 export default router
